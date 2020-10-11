d347850bfd403d8b35be9905060ec7d040737613
soliddefi <soliddefi@gmail.com>
2020-10-11 14:31:38 +0800

    buying and selling simulator

--- simple_price_simulator.py
+++ simple_price_simulator.py
@@ -1,8 +1,12 @@
+"""
+    This program is created in 2020-10-11, it calculates the amount of token a buyer get by bying in a dynamic manner.
+"""
+
 def main():
     x = input("Enter the amount in USDT you are paying:")
     p = input("Enter the current supply of the token:")
     x = float(x)
     p = float(p)
     y = (3/2*x+p**(3/2))**(2/3)-p
-    print(y)
+    print("Amount of token you get:" +str(y))
 main()
