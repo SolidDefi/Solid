a7a0077288f08de3b466e7c01911a4a6099f1c42
soliddefi <soliddefi@gmail.com>
2020-10-11 14:09:59 +0800

    fix typo

--- simple_price_simulator.py
+++ simple_price_simulator.py
@@ -1,6 +1,6 @@
 def main():
     x = input("Enter the amount in USDT you are paying:")
-    p = input("Enter the currply supply of the token:")
+    p = input("Enter the current supply of the token:")
     x = float(x)
     p = float(p)
     y = (3/2*x+p**(3/2))**(2/3)-p
